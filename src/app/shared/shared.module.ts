36d4ed6ac920a337b69335e3b1ca5c1a02701f6c
Sebastian Wesołowski <sebastian.wesolowski.sw@gmail.com>
2020-05-11 08:31:28 +0200

    [#20] #4 - Title component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TitleComponentComponent } from './components/title-component/title-component.component';
+import { ContentComponentComponent } from './components/content-component/content-component.component';
 
 @NgModule({
   imports: [CommonModule, FormsModule, ReactiveFormsModule],
-  declarations: [],
-  exports: []
+  declarations: [TitleComponentComponent, ContentComponentComponent],
+  exports: [TitleComponentComponent]
 })
 export class SharedModule {}
